2425894f2aebb32268e5dbcbf9bfc0aa9607b170
NicolasU94 <nicolas.s.ugarte@gmail.com>
2021-12-01 20:31:23 -0300

    WorkContainer implemented and Db Migration created

---
+++ NT1_BlogAccesoDatos/Data/WorkContainer.cs
@@ -0,0 +1,31 @@
+﻿using NT1_Blog.AccesoDatos.Data;
+using NT1_BlogAccesoDatos.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NT1_BlogAccesoDatos.Data
+{
+    public class WorkContainer : IWorkContainer
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public WorkContainer(ApplicationDbContext db)
+        {
+            _db = db;
+            Categoria = new CategoriaRepository(_db);
+        }
+        public ICategoriaRepository Categoria { get; private set; }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        } 
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
