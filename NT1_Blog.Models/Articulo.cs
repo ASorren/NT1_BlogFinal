1b6e812628939ca1dcb8a99dc7a079d963b8f526
diego boya <dboyatjian@gmail.com>
2021-12-04 15:04:14 -0300

    View and some classes for login

---
+++ NT1_Blog.Models/Articulo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace NT1_Blog.Models
+{
+    public class Articulo
+    {
+        [Key]
+        public int id { get; set; }
+        public string Nombre { get; set; }
+        public string FechaCreacion { get; set; }
+        public string UrlImagen { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
---
+++ NT1_Blog.Models/Slider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace NT1_Blog.Models
+{
+    public class Slider
+    {
+        [Key]
+        public int id { get; set; }
+        [Required(ErrorMessage = "Ingrese un nombre para el slider")]
+        [Display(Name = "Nombre Slider")]
+        public string Nombre { get; set; }
+        [Required]
+        [Display(Name = "Estado")]
+        public bool Estado { get; set; }
+
+        [DataType(DataType.ImageUrl)]
+        [Display(Name ="Imagen")]
+        public string UrlImagen { get; set; }
+        
+    }
+}
